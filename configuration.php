7e977e24adc65c33ac1c8c64867a703ed54298e2
angeldhaliwal <am07anjack@gmail.com>
2024-09-19 15:48:25 +0200

    new

---
+++ configuration.php
@@ -0,0 +1,16 @@
+<?php
+$license = '123123123123';
+$db_host = 'db';
+$db_port = '3306';
+$db_username = 'my_username';
+$db_password = 'my_user_password';
+$db_name = 'my_database_name';
+$db_tls_ca = '';
+$db_tls_ca_path = '';
+$db_tls_cert = '';
+$db_tls_cipher = '';
+$db_tls_key = '';
+$db_tls_verify_cert = '';
+$cc_encryption_hash = 'NgpOL1XRnr77zpkHnPVQ7NikQm5u1iCzyAwOVh1lUzPnA0Hr3d7mYJ7t4NYZQynp';
+$templates_compiledir = 'templates_c';
+$mysql_charset = 'utf8';
